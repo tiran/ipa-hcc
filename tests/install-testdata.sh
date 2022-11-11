dac3247231f3ce2920bc5c07efb72459597e8ba8
Christian Heimes <cheimes@redhat.com>
2022-11-11 13:35:01 +0100

    Test with krb5.conf snippet

--- tests/install-testdata.sh
+++ tests/install-testdata.sh
@@ -5,7 +5,12 @@ set -ex
 ipa-cacert-manage install ./ca/bundle.pem
 ipa-certupdate
 
+# alternative approach
+#cp ca/bundle.pem /etc/ipa/candlepin-bundle.pem
+#cp pkinit_candlepin.conf /etc/krb5.conf.d/pkinit_candlepin
+
 # XXX bug: ipa-certupdate does not restart KDC
 systemctl restart krb5kdc.service
 
 ipa-ldap-updater ./89-testdata.update
+
